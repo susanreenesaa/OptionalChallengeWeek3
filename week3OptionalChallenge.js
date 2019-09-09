672e41874d390424376e43cf7a4269a7581b84d2
reenesa <susannins@gmail.com>
2019-09-09 16:37:37 +0300

    impliment missing number function

--- week3OptionalChallenge.js
+++ week3OptionalChallenge.js
@@ -4,7 +4,24 @@ var number=['1','3','5','8','10'];
 var num1=[];
 var missingNum;
 function findMissingNumbers(numArray){
-    
+    if(numArray[0]>1 || numArray[numArray-1]<1){
+        missingNum=1;
+    }
+    else{
+        for(var i=0; i < numArray.length; i++){
+        
+            if((numArray[i]- numArray[i-1]) >  1){
+                missingNum = parseInt(number[i-1])+ 1;
+                num1.push(missingNum);
+           
+               
+            }
+            
+           
+        }
+     }
+    return ("missing numbers are  " + num1)
+   
 }
 console.log(findMissingNumbers(number))
 console.log(findMissingNumbers([1,5,7,9,12,14]));
