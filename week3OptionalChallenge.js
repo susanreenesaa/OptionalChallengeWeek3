16bc9fceb7fa6d523518652ff4d369c0cc2cedff
reenesa <susannins@gmail.com>
2019-09-09 16:33:44 +0300

    create missing number function

---
+++ week3OptionalChallenge.js
@@ -0,0 +1,10 @@
+// my challenge is that the code prints only one missing number. 
+// if two numbers are missing cocurrently, it prints only one
+var number=['1','3','5','8','10'];
+var num1=[];
+var missingNum;
+function findMissingNumbers(numArray){
+    
+}
+console.log(findMissingNumbers(number))
+console.log(findMissingNumbers([1,5,7,9,12,14]));
