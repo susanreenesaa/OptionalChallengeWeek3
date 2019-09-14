aeb527bb159bb34379712da6157abfa0a4b0e1f3
reenesa <susannins@gmail.com>
2019-09-14 11:35:02 +0300

    change missing function

--- week3OptionalChallenge.js
+++ week3OptionalChallenge.js
@@ -1,20 +1,40 @@
-function missing(arr) {
-    arraySort(arr);
-    const result = [];
-    if (arr.length <= 1) {
-         return result 
+const findTheMissing = (target) => {
+    // final result list
+    arraySort(target);
+    newArray=[...new Set(target)];
+    let result = [];
+    
+    // array will go from min to max value present in the array
+    const min = newArray[0];
+    const max = newArray[newArray.length - 1];
+    
+    // will maintain the track of index of newArray array
+    // will start from 2nd element of array because we need a no. to subsctract from
+    let pivotIndex = 1;
+    
+    for (let index = min; index < max; index++) {
+    
+        // value to the index
+        let pivotValue = newArray[pivotIndex];
+    
+        // dif of the value
+        let diff = pivotValue - index;
+    
+        // diff means its time to move the pivot to next :P
+        if (diff > 0) {
+            // not going to current index at exists in the newArray array
+            if (index === newArray[pivotIndex - 1])
+                index++;
+    
+            // YO!! WE FOUND HE MISSING
+            result.push(index);
+        }
+        else {
+            pivotIndex++;
+        }
     }
-    var i = 1, val = arr[0] + 1;
-    //not sure whatthis does.
-    const count = ((arr[arr.length - 1]) - val) - (arr.length - 2);
-    while (result.length < count) {
-        while (arr[i] !== val) { result.push(val++) }
-        i++;
-        val++;
-    }
-    return result;
+    return result; 
 }
-
 function arraySort(m){
     for (i=0; i<m.length; i++){
     for(var j=i+1; j < m.length; j++){
@@ -27,7 +47,7 @@ function arraySort(m){
 }
 return m
 }
-
-console.log(missing([0,4,6,8]));
-console.log(missing([-2,0,6,-4,3,1]));
-console.log(arraySort([-2,0,6,-4,3,1]));
+var x =[0,4,6,8];
+var y=[-2,0,6,-4,3,3,3,1];
+console.log( "The array is  " + x + " missing numbers are:  " + findTheMissing(x));
+console.log("The array is  " + arraySort(y) + " missing numbers are:  " + findTheMissing(y));
